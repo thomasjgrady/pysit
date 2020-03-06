da69332ca4233eadbd3f1754d1fa699336e9bbda
Thomas Grady <thomasg2@vt.edu>
2020-03-06 12:59:35 -0500

    Fixed commit diff

--- pysit/core/mesh.py
+++ pysit/core/mesh.py
@@ -7,8 +7,6 @@ from pysit.core.domain import RectangularDomain
 from pysit.util.parallel import ParallelWrapCartesian
 from pysit.util.parallel import ParallelWrapCartesianNull
 
-from pysit.util.parallel import ParallelWrapCartesianNull
-
 __all__ = ['MeshBase', 'CartesianMesh',
            'StructuredNeumann', 'StructuredDirichlet', 'StructuredPML']
 
